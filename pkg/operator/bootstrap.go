f7bacbde255ff096138ca1eddfd6b36686451e6a
Martin André <m.andre@redhat.com>
2019-07-25 12:05:58 +0200

    Fix path for manifest files
    Based on https://github.com/celebdor/machine-config-operator/commit/d86e050173c78220b33d8aff4ad7f45b7239ee4c

--- pkg/operator/bootstrap.go
+++ pkg/operator/bootstrap.go
@@ -152,26 +152,27 @@ func RenderBootstrap(
 		name:     "manifests/machineconfigserver/kube-apiserver-serving-ca-configmap.yaml",
 		filename: "manifests/kube-apiserver-serving-ca-configmap.yaml",
 	}}
-	openstackManifests := []struct {
-		name     string
-		data     []byte
-		filename string
-	}{{
-		name:     "manifests/openstack/coredns.yaml",
-		filename: "manifests/coredns.yaml",
-	}, {
-		name:     "manifests/openstack/coredns-corefile.tmpl",
-		filename: "static-pod-resources/coredns/Corefile.tmpl",
-	}, {
-		name:     "manifests/openstack/keepalived.yaml",
-		filename: "manifests/keepalived.yaml",
-	}, {
-		name:     "manifests/openstack/keepalived.conf.tmpl",
-		filename: "static-pod-resources/keepalived/keepalived.conf.tmpl",
-	}}
-	if infra.Status.PlatformStatus.Type == configv1.OpenStackPlatformType {
-		manifests = append(manifests, openstackManifests...)
+
+	if infra.Status.PlatformStatus.OpenStack != nil {
+		manifests = append(manifests, []struct {
+			name     string
+			data     []byte
+			filename string
+		}{{
+			name:     "manifests/openstack/coredns.yaml",
+			filename: "openstack/manifests/coredns.yaml",
+		}, {
+			name:     "manifests/openstack/coredns-corefile.tmpl",
+			filename: "openstack/static-pod-resources/coredns/Corefile.tmpl",
+		}, {
+			name:     "manifests/openstack/keepalived.yaml",
+			filename: "openstack/manifests/keepalived.yaml",
+		}, {
+			name:     "manifests/openstack/keepalived.conf.tmpl",
+			filename: "openstack/static-pod-resources/keepalived/keepalived.conf.tmpl",
+		}}...)
 	}
+
 	for _, m := range manifests {
 		var b []byte
 		var err error
